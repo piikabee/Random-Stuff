5f23c545a8f3bf155d10a43bb400fbbb1f6771a2
piikabee <68369408+piikabee@users.noreply.github.com>
2020-07-17 02:09:11 +0600

    Update main.py

--- Discord Clock/main.py
+++ Discord Clock/main.py
@@ -4,18 +4,19 @@ import datetime
 
 client = discord.Client()
 
-distoken = "Your Discord Token"
+distoken = "NzMzNDE0MDI4MzYxMDcyNjcw.XxCzKA.FWXhoeE-67Am4fwelH-s836vE8M
+"
 
 # These must all be Voice Channels
-timechannel = # The ID of the Channel that gets renamed
+timechannel = 725417791355617341
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     while True:
         now = datetime.datetime.now()
-        await client.get_channel(timechannel).edit(name=f"{now.hour}:{now.minute} (<INSERT YOUR LOCAL TIMEZONE HERE>)") # The channel gets changed here
+        await client.get_channel(timechannel).edit(name=f"{now.hour}:{now.minute} (2:08 AM)") # The channel gets changed here
         await asyncio.sleep(60)
 
 
-client.run(distoken)
+client.run(NzMzNDE0MDI4MzYxMDcyNjcw.XxCzKA.FWXhoeE-67Am4fwelH-s836vE8M)
